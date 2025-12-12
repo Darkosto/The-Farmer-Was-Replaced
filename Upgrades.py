886d7de66a637c6bc1685d3b96a8bfb34c2f2df1
Darkosto <darkosto@darkosto.tv>
2025-12-12 01:21:09 -0800

    Further expands Upgrades

--- Upgrades.py
+++ Upgrades.py
@@ -13,6 +13,18 @@ def maze_unlocked():
 def sunflower_unlocked():
 	return num_unlocked(Unlocks.Sunflowers) > 0
 
+def speed_unlocked():
+	return num_unlocked(Unlocks.Speed) > 0
+
+def fertilizer_unlocked():
+	return num_unlocked(Unlocks.Fertilizer) > 0
+
+def watering_unlocked():
+	return num_unlocked(Unlocks.Watering) > 0
+
+def cactus_unlocked():
+	return num_unlocked(Unlocks.Cactus) > 0
+
 #############################################
 
 def grass_level():
@@ -29,6 +41,18 @@ def tree_level():
 
 def maze_level():
 	return num_unlocked(Unlocks.Mazes)
+
+def speed_level():
+	return num_unlocked(Unlocks.Speed)
+
+def fertilizer_level():
+	return num_unlocked(Unlocks.Fertilizer)
+
+def watering_level():
+	return num_unlocked(Unlocks.Watering)
+
+def cactus_level():
+	return num_unlocked(Unlocks.Cactus)
 
 #############################################
 
@@ -56,6 +80,30 @@ def tree_cost():
 	for item in cost_dict:
 		return cost_dict[item]	
 	
+# Speed Upgrade Requires Hay, Wood, or Carrots
+def speed_cost():
+	cost_dict = get_cost(Unlocks.Speed, speed_level())
+	for item in cost_dict:
+		return cost_dict[item]	
+	
+# Fertilizer Upgrade Requires Wood	
+def fertilizer_cost():
+	cost_dict = get_cost(Unlocks.Fertilizer, fertilizer_level())
+	for item in cost_dict:
+		return cost_dict[item]	
+
+# Watering Upgrade Requires Wood	
+def watering_cost():
+	cost_dict = get_cost(Unlocks.Watering, watering_level())
+	for item in cost_dict:
+		return cost_dict[item]
+
+# Cactus Upgrade Requires Pumpkin	
+def cactus_cost():
+	cost_dict = get_cost(Unlocks.Cactus, cactus_level())
+	for item in cost_dict:
+		return cost_dict[item]	
+	
 #############################################
 
 # Planting Carrots
@@ -71,35 +119,93 @@ available_upgrade_list = {
 	"GrassLevel": grass_level(), 
 	"PumpkinLevel": pumpkin_level(), 
 	"CarrotLevel": carrot_level(),
-	"TreeLevel": tree_level()
+	"TreeLevel": tree_level(),
+	"SpeedLevel": speed_level(),
+	"FertilizerLevel": fertilizer_level(),
+	"WateringLevel": watering_level(),
+	"CactusLevel": cactus_level()	
 }
 
-lowest_value = min([
-	available_upgrade_list["GrassLevel"],
-	available_upgrade_list["PumpkinLevel"],
-	available_upgrade_list["CarrotLevel"],
-	available_upgrade_list["TreeLevel"]
-])
+def upgrade_maxxed(upgrade_name):
+	if upgrade_name == "GrassLevel":
+		return get_cost(Unlocks.Grass) == {}
+	elif upgrade_name == "PumpkinLevel":
+		return get_cost(Unlocks.Pumpkins) == {}
+	elif upgrade_name == "CarrotLevel":
+		return get_cost(Unlocks.Carrots) == {}
+	elif upgrade_name == "TreeLevel":
+		return get_cost(Unlocks.Trees) == {}
+	elif upgrade_name == "SpeedLevel":
+		return get_cost(Unlocks.Speed) == {}
+	elif upgrade_name == "FertilizerLevel":
+		return get_cost(Unlocks.Fertilizer) == {}
+	elif upgrade_name == "WateringLevel":
+		return get_cost(Unlocks.Watering) == {}
+	elif upgrade_name == "CactusLevel":
+		return get_cost(Unlocks.Cactus) == {}		
+	else:
+		return True
 
+lowest_value = None
+lowest_upgrade = None
 for key in available_upgrade_list:
-	if available_upgrade_list[key] == lowest_value:
-		lowest_upgrade = key
-		break
+	if not upgrade_maxxed(key):
+		level = available_upgrade_list[key]
+		if lowest_value == None or level < lowest_value:
+			lowest_value = level
+			lowest_upgrade = key
+
+def grass_upgrade():
+	if grass_cost() < num_items(Items.Wood):
+		print("Unlocking Grass Upgrade")
+		unlock(Unlocks.Grass)
+
+def pumpkin_upgrade():
+	if pumpkin_cost() < num_items(Items.Carrot):
+		print("Unlocking Pumpkin Upgrade")
+		unlock(Unlocks.Pumpkins)
+
+def carrot_upgrade():
+	if carrot_cost() < num_items(Items.Wood):
+		print("Unlocking Carrot Upgrade")
+		unlock(Unlocks.Carrots)
+
+def tree_upgrade():
+	if tree_cost() < num_items(Items.Hay):
+		print("Unlocking Tree Upgrade")
+		unlock(Unlocks.Trees)
+
+def speed_upgrade():
+	if speed_cost() < num_items(Items.Hay):
+		print("Unlocking Speed Upgrade")
+		unlock(Unlocks.Speed)
+
+def fertilizer_upgrade():
+	if fertilizer_cost() < num_items(Items.Wood):
+		print("Unlocking Fertilizer Upgrade")
+		unlock(Unlocks.Fertilizer)
+
+def watering_upgrade():
+	if fertilizer_cost() < num_items(Items.Wood):
+		print("Unlocking Watering Upgrade")
+		unlock(Unlocks.Watering)
+
+def cactus_upgrade():
+	if cactus_cost() < num_items(Items.Pumpkin):
+		print("Unlocking Cactus Upgrade")
+		unlock(Unlocks.Cactus)
 
 # Upgrades List
 # Auto_Unlock
 # Benchmark
 # Cactus
-# Carrots
 # Costs
 # Debug
 # Debug_2
 # Dictionaries
 # Dinosaurs
 # Expand
-# Fertilizer
 # Functions
-# Grass
 # Leaderboard
 # Lists
 # Loops
@@ -108,11 +214,6 @@ for key in available_upgrade_list:
 # Operators
 # Plant
 # Polyculture
-# Pumpkin
 # Senses
-# Speed
-# Sunflowers
-# Trees
 # Utilities
 # Variables
-# Watering
